1a54106abdf1efe314856f4059d990951f247203
Vikash Kumar <nisant54321@gmail.com>
2026-03-19 22:31:14 +0530

    understanding the DOM concept of js part2

--- 03_javascript/07_DOM/script.js
+++ 03_javascript/07_DOM/script.js
@@ -75,6 +75,113 @@ console.log(link.getAttribute("href"));
 link.setAttribute("href", "https://google.com");
 
 
+/*
+------------------------------------------
+6. Creating and adding elements
+------------------------------------------
+*/
+
+// create element
+let newDiv = document.createElement("div");
+
+// add text
+newDiv.innerText = "I am dynamically added";
+
+// add to page
+document.body.appendChild(newDiv);
+
+
+
+/*
+------------------------------------------
+7. Removing elements
+------------------------------------------
+*/
+
+setTimeout(() => {
+    newDiv.remove();
+}, 5000);
+
+
+
+/*
+------------------------------------------
+8. Event Handling
+------------------------------------------
+*/
+
+// using addEventListener (BEST PRACTICE)
+
+button.addEventListener("click", function () {
+    console.log("Button clicked");
+    title.innerText = "Button was clicked";
+});
+
+
+/*
+------------------------------------------
+IMPORTANT DIFFERENCE
+------------------------------------------
+
+1. Using HTML (onclick)
+
+<button onclick="handleClick()"></button>
+
+function handleClick() {
+    console.log("clicked");
+}
+
+Here:
+- function is CALLED from HTML
+- tightly coupled (not good practice)
+
+
+2. Using JS (addEventListener)
+
+button.addEventListener("click", handleClick);
+
+function handleClick() {
+    console.log("clicked");
+}
+
+Here:
+- we only pass function reference (no ())
+- JS handles calling it
+- better separation of code (used in real projects)
+
+
+Wrong:
+button.addEventListener("click", handleClick());
+
+This will run immediately, not on click
+*/
+
+
+
+/*
+9. Input event
+*/
+
+input.addEventListener("input", function () {
+    console.log("User typing:", input.value);
+});
+
+
+
+/*
+10. querySelector (modern way)
+*/
+
+// first matching element
+let firstItem = document.querySelector("li");
+console.log(firstItem.innerText);
+
+// all matching elements (NodeList)
+let allItems = document.querySelectorAll("li");
+
+allItems.forEach(function (item) {
+    console.log(item.innerText);
+});
 
 
 
