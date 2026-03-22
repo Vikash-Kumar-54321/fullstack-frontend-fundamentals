516e556daf337d452c3c8d395104a0bc61f9729e
Vikash Kumar <nisant54321@gmail.com>
2026-03-22 11:19:17 +0530

    understanding the getter and setter in js

--- 03_javascript/15_classPart2.js
+++ 03_javascript/15_classPart2.js
@@ -42,3 +42,50 @@ obj.add() //error
 Because static method is not inside object
 */
 
+
+
+/*
+------------------------------------------
+2. GETTERS AND SETTERS
+------------------------------------------
+
+Used to control access of data
+*/
+
+
+class User {
+    constructor(name) {
+        this._name = name;   // convention: _ means internal use
+    }
+
+    // getter → used like property
+    get name() {
+        return this._name;
+    }
+
+    // setter → used to update value
+    set name(newName) {
+        if (newName.length < 3) {
+            console.log("Name too short");
+            return;
+        }
+        this._name = newName;
+    }
+}
+
+let u1 = new User("Vikash");
+
+/*
+getter works like property, not function
+*/
+console.log(u1.name);
+
+/*
+setter also used like property
+*/
+u1.name = "Vi";   // too short
+u1.name = "Rahul";
+
+console.log(u1.name);
+
+
